754ded45cf5a44a772b9737a8e2f841334daa028
Aditya Rao <aditya.rao@.miqdigital.com>
2020-07-17 01:22:11 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-<<<<<<< HEAD
-=======
-    // TODO: implement createState
->>>>>>> 2d2c8ae89bf87a8e744b267f53afc55ad1d99635
     return MyAppState();
   }
 }
@@ -34,22 +30,8 @@ class MyAppState extends State<MyApp> {
         (dropdown) => dropdown['value'] == crimeInput.toLowerCase());
     print(crimeEntered);
     setState(() {
-<<<<<<< HEAD
       // subDomain = crimeEntered.toString();
       subDomain = crimes.returnCategory();
-=======
-<<<<<<< HEAD
-      // subDomain = crimeEntered.toString();
-      subDomain = crimes.returnCategory();
-=======
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter = _counter + 3;
->>>>>>> 1f1655764ab7c3113b18e71d318f043253f07e70
->>>>>>> 2d2c8ae89bf87a8e744b267f53afc55ad1d99635
     });
   }
 
