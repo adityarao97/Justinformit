f9734596264dc0d7e7c3b9a0cdf5fec4ccb4b97b
tarun.raghunandan <tarun.raghunandan@miqdigital.com>
2020-07-17 05:30:37 +0530

    code refactoring

---
+++ lib/dashboard.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  DashboardState createState() => DashboardState();
+}
+
+class DashboardState extends State<Dashboard> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("JustInformit"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.tune),
+              onPressed: () {
+                print('Filter button pressed');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {
+                print('Settings button pressed');
+              },
+            ),
+          ],
+        ),
+        body: _buildStack());
+  }
+
+  Widget _buildStack() {
+    return Stack(children: <Widget>[
+      Container(
+          alignment: Alignment(-0.75, -0.85),
+          child: Text("Dashboard", style: TextStyle(fontSize: 30))),
+      Container(
+          height: 400,
+          width: 400,
+          alignment: Alignment(-0.75, -0.75),
+          child: _pieChart()),
+      Container(
+        alignment: Alignment(-0.5, 0.25),
+        child: Text(
+          "hello world",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      Container(
+        alignment: Alignment(0.5, 0.25),
+        child: Text(
+          "lorem ipsum",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    ]);
+  }
+
+  Widget _pieChart() {
+    Map<String, double> dataMap = new Map();
+    dataMap.putIfAbsent("Flutter", () => 5);
+    dataMap.putIfAbsent("React", () => 3);
+    dataMap.putIfAbsent("Xamarin", () => 2);
+    dataMap.putIfAbsent("Ionic", () => 2);
+
+    return PieChart(dataMap: dataMap, chartType: ChartType.ring);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:pie_chart/pie_chart.dart';
+import './dashboard.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // void _goToDashboard() {
+  //   Navigator.of(context)
+  //       .push(MaterialPageRoute(builder: (BuildContext context) {
+  //     return Dashboard();
+  //   }));
+  // }
+
   @override
   Widget build(BuildContext context) {
-    Map<String, double> dataMap = new Map();
-    dataMap.putIfAbsent("Flutter", () => 5);
-    dataMap.putIfAbsent("React", () => 3);
-    dataMap.putIfAbsent("Xamarin", () => 2);
-    dataMap.putIfAbsent("Ionic", () => 2);
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.purple[900]),
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("JustInformit"),
-              leading: IconButton(
-                icon: Icon(
-                  Icons.arrow_back,
-                  semanticLabel: 'back',
-                ),
-                onPressed: () {
-                  print("back pressed");
-                },
-              ),
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(
-                    Icons.tune,
-                    semanticLabel: 'tune',
-                  ),
-                  onPressed: () {
-                    print('Filter button pressed');
-                  },
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.settings,
-                    semanticLabel: 'settings',
-                  ),
-                  onPressed: () {
-                    print('Settings button pressed');
-                  },
-                ),
-              ],
-            ),
-            body: Stack(children: <Widget>[
-              Container(
-                  alignment: Alignment(-0.75, -0.85),
-                  child: Text("Dashboard", style: TextStyle(fontSize: 30))),
-              Container(
-                  height: 400,
-                  width: 400,
-                  alignment: Alignment(-0.75, -0.75),
-                  child: PieChart(dataMap: dataMap, chartType: ChartType.ring)),
-              Container(
-                alignment: Alignment(-0.5, 0.25),
-                child: Text(
-                  "hello world",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Container(
-                alignment: Alignment(0.5, 0.25),
-                child: Text(
-                  "lorem ipsum",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ])));
+        theme: ThemeData(primaryColor: Colors.purple[900]), home: Dashboard());
+    // home: Scaffold(
+    //   appBar: AppBar(
+    //     title: Text("JustInformit"),
+    //   ),
+    //   body: RaisedButton(
+    //     child: Text("go to dashboard"),
+    //     onPressed: _goToDashboard,
+    //   ),
+    // ));
   }
 }
