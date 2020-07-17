6e517d0728d933130522443c990bc21419f13f7a
tarun.raghunandan <tarun.raghunandan@miqdigital.com>
2020-07-17 10:03:22 +0530

    firebase intg

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,33 +7,42 @@ class Homepage extends StatefulWidget {
 }
 
 class HomepageState extends State<Homepage> {
+  Widget _buildListItem(BuildContext context) {}
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.home)),
-                Tab(icon: Icon(Icons.grid_on)),
-                Tab(icon: Icon(Icons.chat)),
-              ],
-            ),
-            title: Text('JustInformit'),
-          ),
-          body: TabBarView(
-            children: [
-              Icon(Icons.home),
-              Icon(Icons.grid_on),
-              Icon(Icons.chat),
+        home: DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home)),
+              Tab(icon: Icon(Icons.grid_on)),
+              Tab(icon: Icon(Icons.chat)),
             ],
           ),
-          floatingActionButton: FloatingActionButton,
+          title: Text('JustInformit'),
+        ),
+        body: TabBarView(
+          children: [
+            ListView.builder(
+                itemBuilder: (context, index) => _buildListItem(context)),
+            Icon(Icons.grid_on),
+            Icon(Icons.chat),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (BuildContext context) {
+              return Addreport();
+            }));
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.red,
         ),
       ),
-    );
+    ));
   }
-
-  void addReport() {}
 }
